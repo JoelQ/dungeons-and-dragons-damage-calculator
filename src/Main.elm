424213716808116cd77add3e3f19148d3ad4a80e
Joël Quenneville <joelq@thoughtbot.com>
2023-03-10 17:15:59 -0500

    Add ability to choose advantage/disadvantage

--- src/Main.elm
+++ src/Main.elm
@@ -26,7 +26,15 @@ type alias Flags =
 
 
 type alias Model =
-    { roll : Int }
+    { roll : Int
+    , modifier : AttackModifier
+    }
+
+
+type AttackModifier
+    = Normal
+    | Advantage
+    | Disadvantage
 
 
 init : Flags -> ( Model, Cmd Msg )
@@ -36,7 +44,9 @@ init flags =
 
 initialModel : Model
 initialModel =
-    { roll = 0 }
+    { roll = 0
+    , modifier = Normal
+    }
 
 
 
@@ -46,16 +56,48 @@ initialModel =
 type Msg
     = RollDamage
     | AttackRolled Int
+    | ModifierChosen AttackModifier
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         RollDamage ->
-            ( model, Random.generate AttackRolled d20 )
+            ( model, Random.generate AttackRolled (attack model.modifier) )
 
         AttackRolled newAttack ->
             ( { model | roll = newAttack }, Cmd.none )
+
+        ModifierChosen newModifier ->
+            ( { model | modifier = newModifier }, Cmd.none )
+
+
+attack : AttackModifier -> Generator Int
+attack modifier =
+    case modifier of
+        Normal ->
+            normal
+
+        Advantage ->
+            advantage
+
+        Disadvantage ->
+            disadvantage
+
+
+normal : Generator Int
+normal =
+    d20
+
+
+advantage : Generator Int
+advantage =
+    Random.map2 max d20 d20
+
+
+disadvantage : Generator Int
+disadvantage =
+    Random.map2 min d20 d20
 
 
 d20 : Generator Int
@@ -71,7 +113,36 @@ view : Model -> Html Msg
 view model =
     Html.section []
         [ Html.h1 [] [ Html.text "D&D damage dice roller" ]
-        , Html.button [ Html.Events.onClick RollDamage ] [ Html.text "Roll Attack" ]
+        , Html.form [ Html.Events.onSubmit RollDamage ]
+            [ Html.fieldset []
+                [ Html.legend [] [ Html.text "Modifiers" ]
+                , Html.label [ Html.Attributes.for "normal" ] [ Html.text "Normal" ]
+                , Html.input
+                    [ Html.Attributes.id "normal"
+                    , Html.Attributes.type_ "radio"
+                    , Html.Attributes.checked <| model.modifier == Normal
+                    , Html.Events.onCheck (\_ -> ModifierChosen Normal)
+                    ]
+                    []
+                , Html.label [ Html.Attributes.for "advantage" ] [ Html.text "Advantage" ]
+                , Html.input
+                    [ Html.Attributes.id "advantage"
+                    , Html.Attributes.type_ "radio"
+                    , Html.Attributes.checked <| model.modifier == Advantage
+                    , Html.Events.onCheck (\_ -> ModifierChosen Advantage)
+                    ]
+                    []
+                , Html.label [ Html.Attributes.for "disadvantage" ] [ Html.text "Disadvantage" ]
+                , Html.input
+                    [ Html.Attributes.id "disadvantage"
+                    , Html.Attributes.type_ "radio"
+                    , Html.Attributes.checked <| model.modifier == Disadvantage
+                    , Html.Events.onCheck (\_ -> ModifierChosen Disadvantage)
+                    ]
+                    []
+                ]
+            , Html.button [] [ Html.text "Roll Attack" ]
+            ]
         , Html.div [] [ Html.text <| "Rolled: " ++ String.fromInt model.roll ]
         ]
 
